84b7d174777610824c82a11e7e31ec03bc41687e
Franklin Clark <ffffranklin@users.noreply.github.com>
2025-10-29 02:10:55 -0700

    feat: add shadcn card and implement in item content

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -1,5 +1,24 @@
-export function Item() {
+import { Card, CardContent, CardDescription, CardFooter, CardTitle } from "@/components/ui/card";
+import { PropsWithChildren } from "react";
+
+interface ItemProps {
+  imageUrl: string,
+  title: string,
+  price: string
+}
+
+function Item({imageUrl, title, price}: PropsWithChildren<ItemProps>) {
   return (
-    <div></div>
+    <Card>
+      <CardContent>
+        <img width="300" src={imageUrl || 'https://i.imgur.com/CnhiMyx.jpeg'} />
+      </CardContent>
+      <CardFooter>
+        <CardTitle>{ title }</CardTitle>
+        <CardDescription>{ price }</CardDescription>
+      </CardFooter>
+    </Card>
   )
 }
+
+export { Item }
