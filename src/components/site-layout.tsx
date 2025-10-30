4fbbb5e1f10c745cd9febf57f6cca37e1452eb20
Franklin Clark <ffffranklin@users.noreply.github.com>
2025-10-29 22:23:20 -0700

    feat: update site logo

--- src/components/site-layout.tsx
+++ src/components/site-layout.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export const SiteLayout = ({ children }: React.PropsWithChildren) => {
   return (
     <Fragment>
-      <header className="flex py-4 px-2 mb-4 border-b-1">
+      <header className="flex py-2 px-4 mb-4 border-b-1">
         <Image
-          src={'/illo_starburst-logo-cursive.png'}
+          src={'/illo_starburst-separator-logo.png'}
           alt={'Quasar Creates Logo'}
           width={300}
           height={41}
