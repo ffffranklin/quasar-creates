49432216a1e39b992c5069f6606d07da5241854f
Franklin Clark <ffffranklin@users.noreply.github.com>
2025-10-31 13:39:23 -0700

    refactor: extract and rename items getter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,9 @@
 import {SiteLayout} from "@/components/site-layout";
 import {Product} from "@/components/product";
-import prismaClient from "@/lib/prisma-client";
-
-async function getItems() {
-  const products = await prismaClient.product.findMany({
-    include: {
-      photos: true
-    }
-  })
-  return products.map((product)=> ({
-    id: product.id,
-    imageUrl: product.photos[0].location || '',
-    title: product.title,
-    price: product.content || ''
-  }))
-}
-
+import {getProducts} from "@/lib/product";
 
 export default async function Home() {
-  const products = await getItems();
+  const products = await getProducts();
 
   return (
     <SiteLayout>
