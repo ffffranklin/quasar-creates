1063e6a08ceb8c12ae7d087b8aef3f7d973724fe
Franklin Clark <ffffranklin@users.noreply.github.com>
2025-10-31 17:36:36 -0700

    feat: add edit form

--- src/app/edit/product/[id]/_components/edit-product-form.tsx
+++ src/app/edit/product/[id]/_components/edit-product-form.tsx
@@ -1,5 +1,73 @@
-async function EditProductForm() {
-  return <form>form goes here here...</form>;
+'use client';
+
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
+
+const formSchema = z.object({
+  title: z.string().min(0).max(70),
+  content: z.string().min(0).max(2000),
+});
+
+function EditProductForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: '',
+      content: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // TODO call api client to submit form
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="mb-4">
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Product title" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem className="mb-4">
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Add description here" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type={'submit'}>Submit</Button>
+      </form>
+    </Form>
+  );
 }
 
 export { EditProductForm };
