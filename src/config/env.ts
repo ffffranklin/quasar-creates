f4dbeb94c8e818053b26355ffeb8becbcd9e32aa
Franklin Clark <ffffranklin@users.noreply.github.com>
2025-11-03 15:59:43 -0800

    feat: add env helper

---
+++ src/config/env.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+  API_URL: z.string(),
+});
+
+const env = createEnv();
+
+function createEnv() {
+  const envVars = {
+    API_URL: process.env.NEXT_PUBLIC_API_URL,
+  };
+
+  const parsedEnv = envSchema.safeParse(envVars);
+  if (!parsedEnv.success) {
+    throw new Error(
+      `Invalid env provided.
+  The following variables are missing or invalid:
+  ${formatValidationErrors(parsedEnv.error.flatten().fieldErrors)}
+  `
+    );
+  }
+
+  return parsedEnv.data ?? {};
+}
+
+function formatValidationErrors(errors: unknown) {
+  return Object.entries(errors as { [key: string]: string })
+    .map(([k, v]) => `- ${k}: ${v}`)
+    .join('\n');
+}
+
+export { env };
