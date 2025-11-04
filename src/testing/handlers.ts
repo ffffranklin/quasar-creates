64a501fa62412f7a2b73607e0cb24bdba86000d4
Franklin Clark <ffffranklin@users.noreply.github.com>
2025-11-03 17:21:01 -0800

    refactor: move update products handler to separate var

--- src/testing/handlers.ts
+++ src/testing/handlers.ts
@@ -1,35 +1,35 @@
-import { http, HttpResponse, PathParams } from 'msw';
+import { http, type HttpHandler, HttpResponse, PathParams } from 'msw';
 import { Product } from '@/lib/types';
 import { product } from '@/testing/factories';
 import { env } from '@/config/env';
 
-export const restHandlers = [
-  http.post<PathParams, { method: 'update'; data: Product }>(
-    `${env.API_URL}/api/products/:id`,
-    async ({ request }) => {
-      const req = await request.clone().json();
+const updateProductsHandler: HttpHandler = http.post<
+  PathParams,
+  { method: 'update'; data: Product }
+>(`${env.API_URL}/api/products/:id`, async ({ request }) => {
+  const req = await request.clone().json();
 
-      if (!req.method) {
-        return HttpResponse.json(
-          { error: { message: 'Missing meta HTTP method' } },
-          { status: 400 }
-        );
-      }
+  if (!req.method) {
+    return HttpResponse.json(
+      { error: { message: 'Missing meta HTTP method' } },
+      { status: 400 }
+    );
+  }
 
-      switch (req.method) {
-        case 'update': {
-          const response = product({
-            id: req.data.id,
-            title: req.data.title,
-            content: req.data.content,
-          });
+  switch (req.method) {
+    case 'update': {
+      const response = product({
+        id: req.data.id,
+        title: req.data.title,
+        content: req.data.content,
+      });
 
-          return HttpResponse.json(response);
-        }
-        default: {
-          return new HttpResponse("Couldn't handle request", { status: 400 });
-        }
-      }
+      return HttpResponse.json(response);
     }
-  ),
-];
+    default: {
+      return new HttpResponse("Couldn't handle request", { status: 400 });
+    }
+  }
+});
+
+export const restHandlers = [updateProductsHandler];
